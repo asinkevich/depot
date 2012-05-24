dd43432cc7e68e9f5487112f3113a5897be72efc
Aliaksei Sinkevich <tokefa@gmail.com>
2012-05-24 22:51:51 +0300

    Add tests for validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "title", description: "desc", image_url: "image.jpg")
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |bad_url|
+      assert new_product(bad_url).invalid?, "#{bad_url} shouldn't be valid"
+    end
+  end
+
+  def new_product(image_url)
+    Product.new(title: "title", description: "desc", price: 1, image_url: image_url)
+  end
 end
