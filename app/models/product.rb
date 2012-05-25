2160484579dc248af2a5abdf9cb5475255cd9de4
Aliaksei Sinkevich <tokefa@gmail.com>
2012-05-25 20:02:16 +0300

    Add simple product cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -6,4 +9,15 @@ class Product < ActiveRecord::Base
       with: %r{\.(gif|jpg|png)$}i,
       message: 'URL must point to GIF, JPG or PNG format'
   }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'product still has line items')
+      false
+    end
+  end
 end
