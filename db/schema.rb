c85c16827bb5f1624cb3c2af28a8bca77921bf1c
Aliaksei Sinkevich <tokefa@gmail.com>
2012-05-26 04:57:01 +0300

    Improve product cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120525115805) do
+ActiveRecord::Schema.define(:version => 20120525201832) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120525115805) do
     t.integer  "cart_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
