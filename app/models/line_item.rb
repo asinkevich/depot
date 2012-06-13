cf69ce3eba7c589eb6ffb8af3c1f79148bee5210
KeFA <tokefa@gmail.com>
2012-06-13 20:18:17 +0300

    Update order form

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,8 +1,9 @@
 class LineItem < ActiveRecord::Base
+  belongs_to :order
   belongs_to :cart
   belongs_to :product
 
   def total_price
-    price * quantity
+    product.price * quantity
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: destroy
+
+  PAYMENT_TYPES = ["Check", "Credit cart", "Purchase order"]
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
