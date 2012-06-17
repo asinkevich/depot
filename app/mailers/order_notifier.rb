adb069179664eae4ea6cea2550966398ec4d7566
KeFA <tokefa@gmail.com>
2012-06-18 00:26:11 +0300

    Add basic order submit notification by email

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: "Order submit"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.mail, subject: "Order from shop has been shipped"
+  end
+end
